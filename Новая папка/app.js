6608ea73174200094a2d6e98a6d752a9d8fa163f
Miracle-Roma <110028909+Miracle-Roma@users.noreply.github.com>
2022-08-17 15:43:04 +0500

    new
    project

---
+++ Новая папка/app.js
@@ -0,0 +1,24 @@
+// function salomlash(kim){
+//     return 'Assalomu alaykum ${kim}';
+
+// }
+// console.log(salomlash("Ali"));
+// root.InnerHTML = '${salomlash("Ali")} ${salomlash("Vali")}';
+
+// function salomlash(ism, familiya){
+//     return `Assalomu alaykum ${ism} ${familiya}`;
+// }
+
+// console.log(salomlash("Ali", "Valiyev"));
+
+function salom(){console.log("Salom");}
+salom();
+
+let box = document.getElementById("box");
+
+const admin = "admin";
+const passwodr = "12345";
+
+box.innerHTML = `<h1>Welcom ${admin}</h1>`;
+
+
